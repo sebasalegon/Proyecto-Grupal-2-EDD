b7e0ff3c4695a6528480cb969c7824adb199e600
aaron <aaron@LAPTOP-V6IK050S>
2024-11-22 23:42:34 -0400

    agregando los nodos

--- src/EstructuraDeDatos/NodoFT.java
+++ src/EstructuraDeDatos/NodoFT.java
@@ -2,12 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package EstructuraDeDatos;
+package proyecto2gonzalez;
 
 /**
  *
- * @author sebas
+ * @author Aaron
  */
 public class NodoFT {
-    
+    Integrante integrante;
+    NodoFT[] sons;
+    NodoFT father;
+
+    public NodoFT(Integrante integrante, NodoFT father) {
+        this.integrante = integrante;
+        this.father = father;
+        this.sons = new NodoFT[10];
+    }
 }
