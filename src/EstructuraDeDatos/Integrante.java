fb635a9a8108d8d0f7800b3f13dc5cee3bc31e15
sebasalegon <167771363+sebasalegon@users.noreply.github.com>
2024-11-23 17:26:24 -0400

    Hola soy sebas estoy con jesus montamos el lista simple por su compu porque se le poteo y no hace los commits asi que esta haciendo cosas en la mac y las vamos montando por aca, aqui estan los constructores de integrantes ya vamos a ver los metodos

--- src/EstructuraDeDatos/Integrante.java
+++ src/EstructuraDeDatos/Integrante.java
@@ -8,6 +8,33 @@ package EstructuraDeDatos;
  *
  * @author sebas
  */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Integrante {
-    
+    // Atributos obligatorios
+    public String nombreCompleto;
+    public int ofHisName;
+    public String bornToFather;
+    public String bornToMother;
+    public String ofEyes;
+    public String ofHair;
+
+    // Atributos opcionales
+    public String conocidoComo;
+    public String tituloNobiliario;
+    public String esposa;
+    public ListaSimple hijos;
+    public String notas;
+    public String destino;
+
+    // Constructor
+    public Integrante(String nombreCompleto, int ofHisName, String bornToFather, String ofEyes, String ofHair) {
+        this.nombreCompleto = nombreCompleto;
+        this.ofHisName = ofHisName;
+        this.bornToFather = bornToFather;
+        this.ofEyes = ofEyes;
+        this.ofHair = ofHair;
+        this.hijos = new ListaSimple();
+    }
 }
