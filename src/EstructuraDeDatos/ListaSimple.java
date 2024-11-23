786f5ac52fb3b5544c42fefc225642e8b4b5b92e
sebasalegon <167771363+sebasalegon@users.noreply.github.com>
2024-11-23 16:21:35 -0400

    Hola soy jesus ya listo el ListaSimple necesitamos montar el de integrante

--- src/EstructuraDeDatos/ListaSimple.java
+++ src/EstructuraDeDatos/ListaSimple.java
@@ -9,5 +9,42 @@ package EstructuraDeDatos;
  * @author jesus
  */
 public class ListaSimple {
+    public NodoSL pFirst;
+    private NodoSL pLast;
+
+    public ListaSimple() {
+        this.pFirst = null;
+        this.pLast = null;
+    }
+
+    public void insertar(Integrante integrante) {
+        NodoSL nuevoNodo = new NodoSL(integrante);
+        if (pFirst == null) {
+            pFirst = nuevoNodo;
+            pLast = nuevoNodo;
+        } else {
+            pLast.pNext = nuevoNodo;
+            pLast = nuevoNodo;
+        }
+    }
+
+    public Integrante buscar(String nombreCompleto) {
+        NodoSL actual = pFirst;
+        while (actual != null) {
+            if (actual.integrante.nombreCompleto.equals(nombreCompleto)) {
+                return actual.integrante;
+            }
+            actual = actual.pNext;
+        }
+        return null;
+    }
+
+    public void imprimir() {
+        NodoSL actual = pFirst;
+        while (actual != null) {
+            System.out.println(actual.integrante.nombreCompleto);
+            actual = actual.pNext;
+        }
+    }
     
 }
